6ce19d41c8fd4adad3c03a873d327c3ba12f8ec1
Your Name <sman2d@gmail.com>
2010-04-22 13:03:37 +0400

    some_fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 3
   belongs_to :page
-  
+  default_scope :order => :created_at
   validates_presence_of :title, :content
 end
