2481f85c49f2e1e01eb9cc1584cd429a9d4d0d48
Your Name <sman2d@gmail.com>
2010-04-26 21:29:39 +0400

    qwer

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,7 +2,7 @@ class ClientsController < ApplicationController
 
   def index
     @page = Page.find_by_permalink('clients')
-    @clients = Client.find(:all, :joins => :projects) 
+    @clients = Client.find(:all, :conditions => "image_file_name IS NOT NULL") 
     render :template => 'layouts/clients'
   end
 end
