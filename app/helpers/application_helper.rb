5fdae3d2d1b3abaf39f21c4e77184e0d312bebc9
andreika <sman2d@gmail.com>
2010-04-28 11:41:04 +0400

    a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@
 module ApplicationHelper
   def asset_icon(asset)
     case asset.data_content_type
-      when 'application/pdf': return image_tag ("pdf_icon.gif")
+      when 'application/pdf': return image_tag("pdf_icon.gif")
     end
-    return  image_tag ("asset_icon.gif")
+    return image_tag("asset_icon.gif")
   end
   
   def current_arrow
